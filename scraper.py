134531877f034094f4ae1f276eb652ecb2cf4ca4
Hari-13-04 <hariharanwissend1@gmail.com>
2026-02-27 11:17:27 +0530

    mmtest2

--- scraper.py
+++ scraper.py
@@ -49,7 +49,7 @@ context = browser.new_context(
     viewport={"width": 1920, "height": 1080}
 )
 page = context.new_page()
-stealth(page)
+stealth.stealth_sync(page)
 
 # ============================
 # Scrape function
